3e519314f3bf770a17a9827666899bc6c4b1e5d4
Vrinda6s <111962995+Vrinda6s@users.noreply.github.com>
2024-05-12 16:47:27 +0530

    Update rescale.py

--- rescale.py
+++ rescale.py
@@ -1,13 +1,14 @@
-# we usually resize and rescale video files and images to prevent computational strain. 
-#Large media files tend to store a
-#lot of information in it and displaying it takes up a lot of processing needs that your
-#computer needs to assign. So by resizing and rescaling, we're actually trying to get rid
-#of some of that information. rescaling video implies modifying its height and width to
-#a particular height and width. Generally,it's always best practice to downscale or
-#change the width and height of your video files to a smaller value than the original
-#dimensions. The reason for this is because while most cameras your webcam included, do
-#not support going higher than its maximumcapability. So for example, if a camera shoots
-#in 720 P, chances are it's not going to beable to shoot in 1080 P or higher. 
+"""
+We usually resize and rescale video files and images to prevent computational strain. 
+Large media files tend to store a lot of information in it and displaying it takes 
+up a lot of processing needs that your computer needs to assign. So by resizing and 
+rescaling, we're actually trying to get rid of some of that information. 
+Rescaling video implies modifying its height and width to a particular height and width. 
+Generally,it's always best practice to downscale or change the width and height of your video files 
+to a smaller value than the original dimensions. The reason for this is because most cameras your 
+webcam included, do not support going higher than its maximum capability. So for example, 
+if a camera shoots in 720 P, chances are it's not going to beable to shoot in 1080 P or higher. 
+"""
 
 import cv2 as cv 
 
@@ -23,9 +24,9 @@ def rescaleFrame(frame, scale=0.75):
 capture = cv.VideoCapture('Videos/dog.mp4')
 
 ##resize image:
-# img = cv.imread('Photos/cat.jpg')
-# resized_image = rescaleFrame(img)
-# cv.imshow('Cat', resized_image)
+img = cv.imread('Photos/cat.jpg')
+resized_image = rescaleFrame(img)
+cv.imshow('Cat', resized_image)
 
 def changeRes(width, height):
     # Work only for Live Videos (does not work with already existing video file)
